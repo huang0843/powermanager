add10900193c515cb7261e398307301b59716b20
12617 <huanghui0843@163.com>
2020-01-03 15:59:54 +0800

    提交项目

---
+++ src/main/java/com/neusoft/service/Impl/ManagerSerivice.java
@@ -0,0 +1,38 @@
+package com.neusoft.service.Impl;
+
+import com.neusoft.bean.Manager;
+import com.neusoft.dao.ManagerMapper;
+import com.neusoft.service.IManagerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+@Service
+public class ManagerSerivice implements IManagerService {
+    @Autowired
+    private ManagerMapper mapper;
+
+    public List<Manager> selectAll(Map map) {
+        return mapper.selectAll(map);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public int saveManager(Manager manager) {
+        return mapper.insertSelective(manager);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED,rollbackFor = RuntimeException.class)
+    public void saveManager2(Manager manager) {
+//        try {
+            int rows= mapper.insertSelective(manager);
+            System.out.println("影响的行数"+rows);
+            int t=1/0;
+            throw new RuntimeException();//手动抛出异常
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+    }
+}
